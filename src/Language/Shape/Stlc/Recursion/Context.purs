5695888c58f2c8f3ba99bdd97aad2cf2573d90c3
Henry Blanchette <henry@monkeyface.com>
2022-03-05 11:22:25 -0500

    re-added metadata

--- src/Language/Shape/Stlc/Recursion/Context.purs
+++ src/Language/Shape/Stlc/Recursion/Context.purs
@@ -5,6 +5,7 @@ import Prelude
 import Prim hiding (Type)
 import Data.List (List)
 import Data.Map as Map
+import Language.Shape.Stlc.Metadata (ApplicationTermMetadata, ArrowTypeMetadata, BlockMetadata, ConstructorMetadata, DataDefinitionMetadata, DataTypeMetadata, HoleTermMetadata, LambdaTermMetadata, MatchTermMetadata, ModuleMetadata, TermDefinitionMetadata, HoleTypeMetadata)
 import Language.Shape.Stlc.Recursion.Base as Rec
 import Language.Shape.Stlc.Typing (Context, addDefinitionToContext, addDefinitionsToContext)
 import Undefined (undefined)
@@ -13,21 +14,27 @@ import Unsafe.Error as Unsafe
 -- Recursion principles which handle context & type
 recModule ::
   forall a.
-  { module_ :: List Definition -> Context -> a
+  { module_ :: List Definition -> ModuleMetadata -> Context -> a
   } ->
   Module -> Context -> a
 recModule rec =
   Rec.recModule
-    { module_: \defs meta gamma -> rec.module_ defs (addDefinitionsToContext defs gamma) }
+    { module_:
+        \defs meta gamma ->
+          rec.module_ defs meta (addDefinitionsToContext defs gamma)
+    }
 
 recBlock ::
   forall a.
-  { block :: List Definition -> Term -> Context -> Type -> a
+  { block :: List Definition -> Term -> BlockMetadata -> Context -> Type -> a
   } ->
   Block -> Context -> Type -> a
 recBlock rec =
   Rec.recBlock
-    { block: \defs a meta gamma alpha -> rec.block defs a (addDefinitionsToContext defs gamma) alpha }
+    { block:
+        \defs a meta gamma alpha ->
+          rec.block defs a meta (addDefinitionsToContext defs gamma) alpha
+    }
 
 recDefinitions ::
   forall a.
@@ -40,54 +47,49 @@ recDefinitions rec =
 
 recDefinition ::
   forall a.
-  { term :: TermID -> Type -> Term -> Context -> a
-  , data :: TypeID -> (List Constructor) -> Context -> a
+  { term :: TermID -> Type -> Term -> TermDefinitionMetadata -> Context -> a
+  , data :: TypeID -> (List Constructor) -> DataDefinitionMetadata -> Context -> a
   } ->
   Definition -> Context -> a
-recDefinition rec =
-  Rec.recDefinition
-    { term: \id alpha a meta gamma -> rec.term id alpha a gamma
-    , data: \id constrs meta gamma -> rec.data id constrs gamma
-    }
+recDefinition = Rec.recDefinition
 
 recConstructor ::
   forall a.
-  { constructor :: TermID -> Type -> Context -> a
+  { constructor :: TermID -> Type -> ConstructorMetadata -> Context -> a
   } ->
   Constructor -> Context -> a
-recConstructor rec =
-  Rec.recConstructor
-    { constructor: \id alpha meta gamma -> rec.constructor id alpha gamma }
+recConstructor = Rec.recConstructor
 
 recTerm ::
   forall a.
-  { lambda :: TermID -> Block -> Context -> Type -> a
-  , application :: TermID -> List Term -> Context -> Type -> a
-  , hole :: Context -> Type -> a
-  , match :: TypeID -> Term -> List Term -> Context -> Type -> a
+  { lambda :: TermID -> Block -> LambdaTermMetadata -> Context -> Type -> a
+  , application :: TermID -> List Term -> ApplicationTermMetadata -> Context -> Type -> a
+  , hole :: HoleTermMetadata -> Context -> Type -> a
+  , match :: TypeID -> Term -> List Term -> MatchTermMetadata -> Context -> Type -> a
   } ->
   Term -> Context -> Type -> a
 recTerm rec =
   Rec.recTerm
     { lambda:
         \id block meta gamma alpha -> case alpha of
-          ArrowType beta delta _ -> rec.lambda id block (Map.insert id beta gamma) delta
+          ArrowType beta delta _ -> rec.lambda id block meta (Map.insert id beta gamma) delta
           _ -> Unsafe.error "impossible"
-    , application: \id args meta gamma alpha -> rec.application id args gamma alpha
-    , hole: \meta gamma alpha -> rec.hole gamma alpha
-    , match: \dataID a cases meta gamma alpha -> rec.match dataID a cases gamma alpha
+    , application:
+        \id args meta gamma alpha ->
+          rec.application id args meta gamma alpha
+    , hole:
+        \meta gamma alpha ->
+          rec.hole meta gamma alpha
+    , match:
+        \dataID a cases meta gamma alpha ->
+          rec.match dataID a cases meta gamma alpha
     }
 
 recType ::
   forall a.
-  { arrow :: Type -> Type -> Context -> a
-  , data :: TypeID -> Context -> a
-  , hole :: HoleID -> TypeWeakening -> Context -> a
+  { arrow :: Type -> Type -> ArrowTypeMetadata -> Context -> a
+  , data :: TypeID -> DataTypeMetadata -> Context -> a
+  , hole :: HoleID -> TypeWeakening -> HoleTypeMetadata -> Context -> a
   } ->
   Type -> Context -> a
-recType rec =
-  Rec.recType
-    { arrow: \alpha beta meta gamma -> rec.arrow alpha beta gamma
-    , data: \id meta gamma -> rec.data id gamma
-    , hole: \id wkn meta gamma -> rec.hole id wkn gamma
-    }
+recType = Rec.recType
