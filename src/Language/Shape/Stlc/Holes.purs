0e7f054937a6033af37b6abbdc07f7557794bf5a
Jacob Prinz <jacobeliasprinz@gmail.com>
2022-03-08 15:18:13 -0500

    Hole sub stuff done

--- src/Language/Shape/Stlc/Holes.purs
+++ src/Language/Shape/Stlc/Holes.purs
@@ -6,8 +6,9 @@ import Prim
 import Data.Map (Map, empty, lookup, singleton, union)
 import Data.Maybe (Maybe(..))
 import Data.Set (member)
-import Language.Shape.Stlc.Syntax (Block(..), Case(..), HoleID(..), NeutralTerm(..), Parameter(..), Term(..), Type(..), TypeID(..), TypeWeakening)
+import Language.Shape.Stlc.Syntax (Block(..), Case(..), Constructor(..), Definition(..), HoleID(..), NeutralTerm(..), Parameter(..), Term(..), Type(..), TypeID(..), TypeWeakening)
 import Undefined (undefined)
+import Unsafe (error)
 
 {-
 This file will deal with unification of types, and applying hole substitutions
@@ -17,16 +18,22 @@ to Blocks, Terms, ...
 type HoleSub = Map HoleID Type
 
 subType :: HoleSub -> Type -> Type
-subType sub (ArrowType (Parameter a md1) b md2)
-    = ArrowType (Parameter (subType sub a) md1) (subType sub b) md2
+subType sub (ArrowType param b md2)
+    = ArrowType (subParameter sub param) (subType sub b) md2
 subType sub (DataType i md) = DataType i md
+subType sub (ProxyHoleType i) = error "This probably shouldn't happen"
 subType sub (HoleType id wea md) = case lookup id sub of
     Nothing -> HoleType id wea md
     Just t -> t
 
+subParameter :: HoleSub -> Parameter -> Parameter
+subParameter sub (Parameter t md) = Parameter (subType sub t) md
+
 unifyType :: Type -> Type -> Maybe HoleSub
 unifyType (HoleType id wea md) t2 = Just $ singleton id t2
 unifyType t1 (HoleType id wea md) = unifyType (HoleType id wea md) t1
+unifyType (ProxyHoleType i1) (ProxyHoleType i2)
+    = if i1 == i2 then Just empty else Nothing
 unifyType (ArrowType (Parameter a1 mda1) b1 mdb1) (ArrowType (Parameter a2 mda2) b2 mdb2)
     = do a <- unifyType a1 a2
          b <- unifyType (subType a b1) (subType a b2)
@@ -35,13 +42,26 @@ unifyType (DataType i1 md1) (DataType i2 md2) = if i1 == i2 then Just empty else
 unifyType _ _ = Nothing
 
 subTerm :: HoleSub -> Term -> Term
-subTerm sub (LambdaTerm bind block md) = undefined
+subTerm sub (LambdaTerm bind block md)
+    = LambdaTerm bind (subBlock sub block) md
 subTerm sub (HoleTerm md) = HoleTerm md
 subTerm sub (MatchTerm id t cases md)
     = MatchTerm id (subTerm sub t) (map (subCase sub) cases) md
-subTerm sub (NeutralTerm t md) = NeutralTerm (subTerm sub t) md
+subTerm sub (NeutralTerm t md) = NeutralTerm (subNeutral sub t) md
 
 subBlock :: HoleSub -> Block -> Block
+subBlock sub (Block defs t md)
+    = Block (map (subDefinition sub) defs) (subTerm sub t) md
+
+subDefinition :: HoleSub -> Definition -> Definition
+subDefinition sub (TermDefinition binds ty t md)
+    = TermDefinition binds (subType sub ty) (subTerm sub t) md
+subDefinition sub (DataDefintion bind ctrs md)
+    = DataDefintion bind (map (subConstructor sub) ctrs) md
+
+subConstructor :: HoleSub -> Constructor -> Constructor
+subConstructor sub (Constructor bind args md)
+    = Constructor bind (map (subParameter sub) args) md
 
 subCase :: HoleSub -> Case -> Case
 subCase sub (Case binds t md) = undefined
