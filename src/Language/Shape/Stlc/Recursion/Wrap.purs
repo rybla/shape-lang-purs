66c33ed1582343c10bc03010369dc0e7c70d1c6d
Henry Blanchette <henry@monkeyface.com>
2022-03-09 09:50:45 -0500

    wrote wrapping logic without handling typechanges & holesubs

--- src/Language/Shape/Stlc/Recursion/Wrap.purs
+++ src/Language/Shape/Stlc/Recursion/Wrap.purs
@@ -34,7 +34,7 @@ recModule rec =
     { module_:
         \defs meta gamma metaGamma wrap_mod ->
           rec.module_ defs meta gamma metaGamma
-            $ \i def' -> wrap_mod $ Module (List.updateAt' i def' defs) meta
+            (\i def' -> wrap_mod $ Module (List.updateAt' i def' defs) meta)
     }
 
 recBlock ::
@@ -47,7 +47,7 @@ recBlock rec =
     { block:
         \defs a meta gamma alpha metaGamma wrap_block ->
           rec.block defs a meta gamma alpha metaGamma
-            $ \i -> wrap_block <<< \def' -> Block (List.updateAt' i def' defs) a meta
+            (\i -> wrap_block <<< \def' -> Block (List.updateAt' i def' defs) a meta)
     }
 
 recDefinition ::
@@ -59,14 +59,14 @@ recDefinition ::
 recDefinition rec =
   Rec.recDefinition
     { term:
-        \x alpha a meta gamma metaGamma wrap_def ->
-          rec.term x alpha a meta gamma metaGamma
-            (wrap_def <<< \alpha' -> TermDefinition x alpha' a meta)
-            (wrap_def <<< \a' -> TermDefinition x alpha a' meta)
+        \termBnd alpha a meta gamma metaGamma wrap_def ->
+          rec.term termBnd alpha a meta gamma metaGamma
+            (wrap_def <<< \alpha' -> TermDefinition termBnd alpha' a meta)
+            (wrap_def <<< \a' -> TermDefinition termBnd alpha a' meta)
     , data:
-        \x constrs meta gamma metaGamma wrap_def ->
-          rec.data x constrs meta gamma metaGamma
-            (\i -> wrap_def <<< \constr' -> DataDefinition x (List.updateAt' i constr' constrs) meta)
+        \typeBnd constrs meta gamma metaGamma wrap_def ->
+          rec.data typeBnd constrs meta gamma metaGamma
+            (\i -> wrap_def <<< \constr' -> DataDefinition typeBnd (List.updateAt' i constr' constrs) meta)
     }
 
 recConstructor ::
@@ -74,7 +74,13 @@ recConstructor ::
   { constructor :: TermBinding -> List Parameter -> ConstructorMetadata -> Context -> TypeBinding -> MetaContext -> IndexWrap Parameter -> a
   } ->
   Constructor -> Context -> TypeBinding -> MetaContext -> Wrap Constructor -> a
-recConstructor rec = Rec.recConstructor undefined
+recConstructor rec =
+  Rec.recConstructor
+    { constructor:
+        \termBnd prms meta gamma typeBnd metaGamma wrap_constr ->
+          rec.constructor termBnd prms meta gamma typeBnd metaGamma
+            (\i -> wrap_constr <<< \prm' -> Constructor termBnd (List.updateAt' i prm' prms) meta)
+    }
 
 recType ::
   forall a.
@@ -84,7 +90,17 @@ recType ::
   , proxyHole :: HoleID -> Context -> MetaContext -> a
   } ->
   Type -> Context -> MetaContext -> Wrap Type -> a
-recType rec = Rec.recType undefined
+recType rec =
+  Rec.recType
+    { arrow:
+        \prm beta meta gamma metaGamma wrap_arr ->
+          rec.arrow prm beta meta gamma metaGamma
+            (wrap_arr <<< \prm' -> ArrowType prm' beta meta)
+            (wrap_arr <<< \beta' -> ArrowType prm beta' meta)
+    , data: \typeID meta gamma metaGamma _ -> rec.data typeID meta gamma metaGamma
+    , hole: \holeID wkn meta gamma metaGamma _ -> rec.hole holeID wkn meta gamma metaGamma
+    , proxyHole: \holeID gamma metaGamma _ -> rec.proxyHole holeID gamma metaGamma
+    }
 
 recTerm ::
   forall a.
@@ -94,7 +110,25 @@ recTerm ::
   , match :: TypeID -> Term -> List Case -> MatchTermMetadata -> Context -> Type -> MetaContext -> Wrap Term -> IndexWrap Case -> a
   } ->
   Term -> Context -> Type -> MetaContext -> Wrap Term -> a
-recTerm rec = Rec.recTerm undefined
+recTerm rec =
+  Rec.recTerm
+    { lambda:
+        \termBnd block meta gamma alpha metaGamma wrap_term ->
+          rec.lambda termBnd block meta gamma alpha metaGamma
+            (wrap_term <<< \block' -> LambdaTerm termBnd block' meta)
+    , neutral:
+        \neu meta gamma alpha metaGamma wrap_term ->
+          rec.neutral neu meta gamma alpha metaGamma
+            (wrap_term <<< \neu' -> NeutralTerm neu' meta)
+    , hole:
+        \meta gamma alpha metaGamma _ ->
+          rec.hole meta gamma alpha metaGamma
+    , match:
+        \typeID a cases meta gamma alpha metaGamma wrap_term ->
+          rec.match typeID a cases meta gamma alpha metaGamma
+            (wrap_term <<< \a' -> MatchTerm typeID a' cases meta)
+            (\i -> wrap_term <<< \case' -> MatchTerm typeID a (List.updateAt' i case' cases) meta)
+    }
 
 recNeutralTerm ::
   forall a.
@@ -102,16 +136,38 @@ recNeutralTerm ::
   , application :: NeutralTerm -> Term -> ApplicationTermMetadata -> Context -> Parameter -> Type -> MetaContext -> Wrap NeutralTerm -> Wrap Term -> a
   } ->
   NeutralTerm -> Context -> Type -> MetaContext -> Wrap NeutralTerm -> a
-recNeutralTerm rec = Rec.recNeutralTerm undefined
+recNeutralTerm rec =
+  Rec.recNeutralTerm
+    { variable:
+        \termID meta gamma alpha metaGamma _ ->
+          rec.variable termID meta gamma alpha metaGamma
+    , application:
+        \neu a meta gamma prm alpha metaGamma wrap_neu ->
+          rec.application neu a meta gamma prm alpha metaGamma
+            (wrap_neu <<< \neu' -> ApplicationTerm neu' a meta)
+            (wrap_neu <<< \a' -> ApplicationTerm neu a' meta)
+    }
 
 recCase ::
   forall a.
   { case_ :: List TermBinding -> Term -> CaseMetadata -> Context -> Type -> TypeID -> TermID -> MetaContext -> Wrap Term -> a } ->
   Case -> Context -> Type -> MetaContext -> Wrap Case -> a
-recCase rec = Rec.recCase undefined
+recCase rec =
+  Rec.recCase
+    { case_:
+        \termBnds a meta gamma alpha typeID termID metaGamma wrap_case ->
+          rec.case_ termBnds a meta gamma alpha typeID termID metaGamma
+            (wrap_case <<< \a' -> Case termBnds a' meta)
+    }
 
 recParameter ::
   forall a.
   { parameter :: Type -> ParameterMetadata -> Context -> MetaContext -> Wrap Type -> a } ->
   Parameter -> Context -> MetaContext -> Wrap Parameter -> a
-recParameter rec = Rec.recParameter undefined
+recParameter rec =
+  Rec.recParameter
+    { parameter:
+        \alpha meta gamma metaGamma wrap_prm ->
+          rec.parameter alpha meta gamma metaGamma
+            (wrap_prm <<< \alpha -> Parameter alpha meta)
+    }
