ff43de32a5e4a08c937f4060a23779b944e4c57b
Henry Blanchette <henry@monkeyface.com>
2022-03-11 17:02:06 -0500

    beautiful index

--- src/Language/Shape/Stlc/Index.purs
+++ src/Language/Shape/Stlc/Index.purs
@@ -1,55 +1,33 @@
 module Language.Shape.Stlc.Index where
 
-data ModuleIndex
-  = ModuleIndex_Here
-  | ModuleIndex_Definition Int DefinitionIndex
+type Index
+  = Array IndexStep
 
-data BlockIndex
-  = BlockIndex_Here
-  | BlockIndex_Definition Int DefinitionIndex
-  | BlockIndex_Term TermIndex
-
-data DefinitionIndex
-  = DefinitionIndex_Here
-  | TermDefinitionIndex_TermBinding
-  | TermDefinitionIndex_Type TypeIndex
-  | TermDefinitionIndex_Term TermIndex
-  | DataDefinitionIndex_TypeBinding
-  | DataDefinitionIndex_Constructor Int ConstructorIndex
-
-data ConstructorIndex
-  = ConstructorIndex_TermBinding
-  | ConstructorIndex_Parameter Int ParameterIndex
-
-data TermIndex
-  = TermIndex_Here
-  | LambdaTermIndex_TermId
-  | LambdaTermIndex_Block BlockIndex
-  | HoleTermIndex
-  | MatchTermIndex_TypeId
-  | MatchTermIndex_Term TermIndex
-  | MatchTermIndex_Case Int CaseIndex
-  | NeutralTermIndex_TermId
-  | NeutralTermIndex_Args ArgsIndex
-
-data ArgsIndex
-  = NoneArgsIndex
-  | ConsArgsIndex_Term TermIndex
-  | ConsArgsIndex_Args ArgsIndex
-
-data CaseIndex
-  = CaseIndex_Here
-  | CaseIndex_TermId Int
-  | CaseIndex_Term TermIndex
-
-data TypeIndex
-  = TypeIndex_Here
-  | ArrowTypeIndex_Parameter ParameterIndex
-  | ArrowTypeIndex_Type TypeIndex
-  | DataTypeIndex_Here
-  | HoleTypeIndex_Here
-  | ProxyHoleTypeIndex_Here
-
-data ParameterIndex
-  = ParameterIndex_Here
-  | ParameterIndex_Type TypeIndex
+data IndexStep
+  = Module_Definition Int
+  | Block_Definition Int
+  | Block_Term
+  | TermDefinition_TermBinding
+  | TermDefinition_Type
+  | TermDefinition_Term
+  | DataDefinition_TypeBinding
+  | DataDefinition_Constructor Int
+  | Constructor_TermBinding
+  | Constructor_Parameter Int
+  | LambdaTerm_TermId
+  | LambdaTerm_Block
+  | HoleTerm
+  | MatchTerm_TypeId
+  | MatchTerm_Term
+  | MatchTerm_Case Int
+  | NeutralTerm_TermId
+  | NeutralTerm_Args
+  | NoneArgs
+  | ConsArgs_Term
+  | ConsArgs_Args
+  | Case_TermId
+  | Case_Term
+  | ArrowType_Parameter
+  | ArrowType_Type
+  | Parameter_Type
+  | Here
