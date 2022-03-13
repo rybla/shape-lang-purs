4997abeff929509cbca188d53d4e7687f30406d8
Henry Blanchette <henry@monkeyface.com>
2022-03-13 16:40:58 -0400

    modifySyntaxAt

--- src/Language/Shape/Stlc/Index.purs
+++ src/Language/Shape/Stlc/Index.purs
@@ -50,38 +50,51 @@ instance Eq IndexStep where eq step step' = genericEq step step'
 pushIndex :: Index -> IndexStep -> Index
 pushIndex = snoc
 
+infix 5 pushIndex as :>
+
+modifySyntaxAt :: Index -> (Syntax -> Syntax) -> Module -> Module 
+modifySyntaxAt ix f mod = mod
+
+-- TODO
+-- setMetadataAt :: forall a. Index -> a -> Module -> Module 
+-- setMetadataAt ix meta' = goModule 
+  -- where 
+  -- l = length ix
+  -- goModule i (Module defs meta) =
+  --   if i == l - 1
+  --     then Module defs (unsafeCoerce meta')
+  --     else case index' ix i of
+  --       Module_Definition j -> Module (List.updateAt' j (goDefinition (i + 1) (List.index' defs j)) defs) meta
+  --       _ -> error "impossible"
+  -- goDefinition i (TermDefinition termBinding alpha a meta) =
+  --   if i == l - 1
+  --     then TermDefinition termBinding alpha a $ unsafeCoerce meta'
+  --     else case index' ix i of 
+  --       TermDefinition_TermBinding -> TermDefinition (goTermBinding (i + 1) termBinding) alpha a meta
+  --       TermDefinition_Type -> TermDefinition termBinding (goType (i + 1) alpha) a meta
+  --       TermDefinition_Term -> TermDefinition termBinding alpha (goTerm (i + 1) a) meta
+  --       _ -> error "impossible"
+  -- goDefinition i (DataDefinition typeBinding constrs meta) = undefined
+  -- goType = undefined 
+  -- goTerm = undefined 
+  -- goTermBinding = undefined 
+
+
 data Direction = Up | Down | Left | Right
 
 moveIndex :: Direction -> Index -> Index 
 moveIndex dir ix = ix 
 
-moveIndexUp :: Index -> Index 
-moveIndexUp ix = case unsnoc ix of 
+moveIndexUp :: Module -> Index -> Index 
+moveIndexUp _ ix = case unsnoc ix of 
   Nothing -> []
   Just {init: ix'} -> ix' 
 
-infix 5 pushIndex as :>
+moveIndexLeft :: Module -> Index -> Index 
+moveIndexLeft mod ix = ix 
 
--- TODO
-setMetadataAt :: forall a. Index -> a -> Module -> Module 
-setMetadataAt ix meta' = goModule 0 
-  where 
-  l = length ix
-  goModule i (Module defs meta) =
-    if i == l - 1
-      then Module defs (unsafeCoerce meta')
-      else case index' ix i of
-        Module_Definition j -> Module (List.updateAt' j (goDefinition (i + 1) (List.index' defs j)) defs) meta
-        _ -> error "impossible"
-  goDefinition i (TermDefinition termBinding alpha a meta) =
-    if i == l - 1
-      then TermDefinition termBinding alpha a $ unsafeCoerce meta'
-      else case index' ix i of 
-        TermDefinition_TermBinding -> TermDefinition (goTermBinding (i + 1) termBinding) alpha a meta
-        TermDefinition_Type -> TermDefinition termBinding (goType (i + 1) alpha) a meta
-        TermDefinition_Term -> TermDefinition termBinding alpha (goTerm (i + 1) a) meta
-        _ -> error "impossible"
-  goDefinition i (DataDefinition typeBinding constrs meta) = undefined
-  goType = undefined 
-  goTerm = undefined 
-  goTermBinding = undefined 
+moveIndexRight :: Module -> Index -> Index 
+moveIndexRight mod ix = ix 
+
+moveIndexDown :: Module -> Index -> Index 
+moveIndexDown mod ix = ix 
