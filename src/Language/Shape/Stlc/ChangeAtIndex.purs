f378c1a54dc4cfeca87db6b4c30d04c949958622
Jacob Prinz <jacobeliasprinz@gmail.com>
2022-03-18 21:52:31 -0400

    Improved highlight/select css slightly

--- src/Language/Shape/Stlc/ChangeAtIndex.purs
+++ src/Language/Shape/Stlc/ChangeAtIndex.purs
@@ -1,4 +1,5 @@
 module Language.Shape.Stlc.ChangeAtIndex where
+{-
 
 import Data.Tuple.Nested
 
@@ -24,18 +25,20 @@ chAtModule = undefined
 
 -- chAtTerm = undefined
 
-chAtTerm :: Rec.RecTerm (Syntax -> TypeChange -> DownwardIndex -> Term /\ DownwardIndex /\ TypeChange)
-chAtTerm = Rec.recTerm {
-    lambda : \termId block meta gamma param beta tRep sbjto -> ?h -- case _ of
-        -- (Cons (IndexStep StepBlock i) rest) -> ?h
-        -- _ -> ?h
-    , neutral : \termId argItems meta gamma alpha -> ?h
-    , hole : \meta gamma alpha -> ?h
-    , match : \dataID a cases meta gamma alpha -> ?h
-}
+-- chAtTerm :: Rec.RecTerm (Syntax -> TypeChange -> DownwardIndex -> Term /\ DownwardIndex /\ TypeChange)
+-- chAtTerm = Rec.recTerm {
+--     lambda : \termId block meta gamma param beta tRep sbjto -> ?h -- case _ of
+--         -- (Cons (IndexStep StepBlock i) rest) -> ?h
+--         -- _ -> ?h
+--     , neutral : \termId argItems meta gamma alpha -> ?h
+--     , hole : \meta gamma alpha -> ?h
+--     , match : \dataID a cases meta gamma alpha -> ?h
+-- }
 
 chAtBlock :: Rec.RecBlock (DownwardIndex -> Syntax -> TypeChange -> Term /\ DownwardIndex /\ TypeChange)
 chAtBlock = undefined
+
+-}
 
 -- I should use the Context recursor in order to deal with keeping track of the
 -- types and context.
