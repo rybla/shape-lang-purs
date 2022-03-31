59df6f033af85e3ab4a935563894e2971b0b380d
Henry Blanchette <henry@monkeyface.com>
2022-03-31 15:32:44 -0400

    pretty-printing

--- src/Language/Shape/Stlc/Syntax.purs
+++ src/Language/Shape/Stlc/Syntax.purs
@@ -5,13 +5,18 @@ import Language.Shape.Stlc.Metadata
 import Prelude
 import Prim hiding (Type)
 
+import Data.Array as Array
 import Data.Generic.Rep (class Generic)
 import Data.List (List)
+import Data.List as List
 import Data.Set (Set)
 import Data.Show.Generic (genericShow)
+import Data.String as String
 import Data.Tuple (fst)
 import Data.UUID (UUID, genUUID)
 import Effect.Unsafe (unsafePerformEffect)
+import Partial.Unsafe (unsafeCrashWith)
+import Undefined (undefined)
 import Unsafe as Unsafe
 
 data Module
@@ -150,19 +155,65 @@ derive instance Generic TypeId  _
 derive instance Generic HoleId _
 
 -- Show instances
-instance Show Module where show x = genericShow x 
-instance Show Block where show x = genericShow x 
-instance Show Definition where show x = genericShow x 
-instance Show Constructor where show x = genericShow x 
-instance Show Type where show x = genericShow x 
-instance Show Term where show x = genericShow x 
-instance Show Case where show x = genericShow x 
-instance Show Parameter where show x = genericShow x 
-instance Show TypeBinding where show x = genericShow x 
-instance Show TermBinding where show x = genericShow x 
-instance Show TermId where show x = genericShow x 
-instance Show TypeId  where show x = genericShow x 
-instance Show HoleId where show x = genericShow x 
+
+unwords = Array.intercalate " " 
+unwordsL = List.intercalate " " 
+
+instance Show Module where 
+  show (Module defItems _) = List.intercalate "; " (showItem <$> defItems)
+
+instance Show Block where 
+  show (Block defItems a _) = List.intercalate "; " (showItem <$> defItems) <> "; " <> show a 
+
+instance Show Definition where 
+  show = case _ of 
+    TermDefinition termBnd type_ term _ -> unwords [ show termBnd, ":", show type_, "=" , show term ]
+    DataDefinition typeBnd constrItems _  -> unwords [ show typeBnd, "::=" , "{", unwordsL $ showItem <$> constrItems, "}" ]
+
+instance Show Constructor where 
+  show (Constructor termBnd prmItems _) = unwords [ "|", show termBnd, "of", unwordsL $ showItem <$> prmItems ]
+
+instance Show Type where 
+  show = case _ of 
+    ArrowType prm beta _ -> unwords [ "(", show prm, "->", show beta, ")"]
+    DataType typeId _ -> show typeId 
+    HoleType holeId wkn _ -> show holeId 
+    ProxyHoleType holeId -> "Proxy(" <>  show holeId <> ")"
+
+instance Show Term where 
+  show = case _ of 
+    LambdaTerm termId  block _ -> unwords ["(", show termId, "=>", show block, ")"]
+    NeutralTerm termId argItems _ -> unwords ["(", show termId, unwordsL $ showItem <$> argItems ]
+    HoleTerm _ -> "?"
+    MatchTerm typeId term caseItems _ -> unwords ["(", "match", show term, ":", show typeId, "with", unwordsL $ showItem <$> caseItems, ")"]
+    
+instance Show Case where 
+  show (Case termIdItems block _ ) = unwords ["|", unwordsL $ showItem <$> termIdItems, "=>", show block]
+
+instance Show Parameter where 
+  show (Parameter alpha meta) = unwords ["(", show meta.name, ":", show alpha, ")"]
+
+instance Show TypeBinding where 
+  show (TypeBinding typeId meta) = show meta.name 
+
+instance Show TermBinding where 
+  show (TermBinding termId meta) = show meta.name 
+
+instance Show TermId where 
+  show (TermId uuid) = "TermId("  <> showUUID uuid <> ")"
+
+instance Show TypeId  where 
+  show (TypeId uuid) = "TypeId(" <> showUUID uuid  <> ")"
+
+instance Show HoleId where 
+  show (HoleId uuid) = "HoleId(" <> showUUID uuid  <> ")" 
+
+showItem = show <<< fst 
+
+showUUID :: UUID -> String 
+showUUID uuid = 
+  let s = show uuid in 
+  String.take 4 $ String.drop 6 $ String.take (String.length s - 6)$ s 
 
 -- Eq instances
 derive instance Eq Module
@@ -211,72 +262,72 @@ instance Show Syntax where show x = genericShow x
 
 toModule :: Syntax -> Module
 toModule (SyntaxModule mod) = mod
-toModule _ = Unsafe.error "impossible cast from Syntax"
+toModule syn = unsafeCrashWith $ "impossible cast from Syntax: " <> show syn
 
 toBlock :: Syntax -> Block
 toBlock (SyntaxBlock block) = block
-toBlock _ = Unsafe.error "impossible cast from Syntax"
+toBlock _ = unsafeCrashWith "impossible cast from Syntax"
 
 toDefinition :: Syntax -> Definition
 toDefinition (SyntaxDefinition def) = def 
-toDefinition _ = Unsafe.error "impossible cast from Syntax"
+toDefinition _ = unsafeCrashWith "impossible cast from Syntax"
 
 toConstructor :: Syntax -> Constructor
 toConstructor (SyntaxConstructor constr) = constr
-toConstructor _ = Unsafe.error "impossible cast from Syntax"
+toConstructor _ = unsafeCrashWith "impossible cast from Syntax"
 
 toTerm :: Syntax -> Term
 toTerm (SyntaxTerm a)  = a 
-toTerm _ = Unsafe.error "impossible cast from Syntax"
+toTerm _ = unsafeCrashWith "impossible cast from Syntax"
 
 toCase :: Syntax -> Case
 toCase (SyntaxCase case_) = case_ 
-toCase _ = Unsafe.error "impossible cast from Syntax"
+toCase _ = unsafeCrashWith "impossible cast from Syntax"
 
 toType :: Syntax -> Type
 toType (SyntaxType alpha) = alpha
-toType _ = Unsafe.error "impossible cast from Syntax"
+toType _ = unsafeCrashWith "impossible cast from Syntax"
 
 toParameter :: Syntax -> Parameter
 toParameter (SyntaxParameter param) = param 
-toParameter _ = Unsafe.error "impossible cast from Syntax"
+toParameter _ = unsafeCrashWith "impossible cast from Syntax"
 
 toTermBinding :: Syntax -> TermBinding
 toTermBinding (SyntaxTermBinding termBinding) = termBinding
-toTermBinding _ = Unsafe.error "impossible cast from Syntax"
+toTermBinding _ = unsafeCrashWith "impossible cast from Syntax"
 
 toTypeBinding :: Syntax -> TypeBinding
 toTypeBinding (SyntaxTypeBinding typeBinding) = typeBinding
-toTypeBinding _ = Unsafe.error "impossible cast from Syntax"
+toTypeBinding _ = unsafeCrashWith "impossible cast from Syntax"
 
 toTermId :: Syntax -> TermId
 toTermId (SyntaxTermId termId) = termId 
-toTermId _ = Unsafe.error "impossible cast from Syntax"
+toTermId _ = unsafeCrashWith "impossible cast from Syntax"
 
 toDefinitionItem :: Syntax -> DefinitionItem
 toDefinitionItem (SyntaxDefinitionItem defItem) = defItem
-toDefinitionItem _ = Unsafe.error "impossible to cast from Syntax"
+toDefinitionItem _ = unsafeCrashWith "impossible to cast from Syntax"
 
 toConstructorItem :: Syntax -> ConstructorItem
 toConstructorItem (SyntaxConstructorItem defItem) = defItem
-toConstructorItem _ = Unsafe.error "impossible to cast from Syntax"
+toConstructorItem _ = unsafeCrashWith "impossible to cast from Syntax"
 
 toParameterItem :: Syntax -> ParameterItem
 toParameterItem (SyntaxParameterItem defItem) = defItem
-toParameterItem _ = Unsafe.error "impossible to cast from Syntax"
+toParameterItem _ = unsafeCrashWith "impossible to cast from Syntax"
 
 toCaseItem :: Syntax -> CaseItem
 toCaseItem (SyntaxCaseItem defItem) = defItem
-toCaseItem _ = Unsafe.error "impossible to cast from Syntax"
+toCaseItem _ = unsafeCrashWith "impossible to cast from Syntax"
 
 toArgItem :: Syntax -> ArgItem
 toArgItem (SyntaxArgItem defItem) = defItem
-toArgItem _ = Unsafe.error "impossible to cast from Syntax"
+toArgItem _ = unsafeCrashWith "impossible to cast from Syntax"
 
 toTermIdItem :: Syntax -> TermIdItem
 toTermIdItem (SyntaxTermIdItem defItem) = defItem
-toTermIdItem _ = Unsafe.error "impossible to cast from Syntax"
+toTermIdItem _ = unsafeCrashWith "impossible to cast from Syntax"
 
 toSyntaxList :: Syntax -> List Syntax 
 toSyntaxList (SyntaxList syns) = syns
-toSyntaxList _ = Unsafe.error "impossible to cast from Syntax"
+toSyntaxList _ = unsafeCrashWith "impossible to cast from Syntax"
