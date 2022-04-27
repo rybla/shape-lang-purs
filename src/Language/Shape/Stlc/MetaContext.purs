e9ba0deb0ded63d0121333980348f8fe7182f7b0
Henry Blanchette <henry@monkeyface.com>
2022-04-27 16:13:51 -0400

    shadows

--- src/Language/Shape/Stlc/MetaContext.purs
+++ src/Language/Shape/Stlc/MetaContext.purs
@@ -1,11 +1,12 @@
 module Language.Shape.Stlc.MetaContext where
 
+import Data.Newtype
 import Language.Shape.Stlc.Metadata
 import Language.Shape.Stlc.Syntax
 import Prelude
 import Data.Map (Map)
 import Data.Map as Map
-import Data.Newtype
+import Data.Maybe (Maybe(..), maybe)
 import Data.Newtype as NT
 import Data.Set (Set)
 import Data.Set as Set
@@ -16,7 +17,9 @@ import Undefined (undefined)
 newtype MetaContext
   = MetaContext
   { varNames :: Map TermId Name -- varId => varName
+  , varShadows :: Map Name Int -- varName => shadowDepth
   , dataNames :: Map TypeId Name -- dataId => dataName
+  , dataShadows :: Map Name Int -- dataName => shadowDepth
   , indentation :: Int
   }
 
@@ -24,15 +27,25 @@ derive instance newTypeMetaContext :: Newtype MetaContext _
 
 _varNames = Proxy :: Proxy "varNames"
 
+_varShadows = Proxy :: Proxy "varShadows"
+
 _dataNames = Proxy :: Proxy "dataNames"
+
+_dataShadows = Proxy :: Proxy "dataShadows"
 
 _indentation = Proxy :: Proxy "indentation"
 
 insertVarName :: TermId -> Name -> MetaContext -> MetaContext
-insertVarName id name = over MetaContext $ Record.modify _varNames (Map.insert id name)
+insertVarName termId name =
+  over MetaContext
+    $ Record.modify _varNames (Map.insert termId name)
+    <<< Record.modify _varShadows (Map.alter (maybe (Just 0) (Just <<< (1 + _))) name)
 
 insertDataName :: TypeId -> Name -> MetaContext -> MetaContext
-insertDataName id name = over MetaContext $ Record.modify _dataNames (Map.insert id name)
+insertDataName id name =
+  over MetaContext
+    $ Record.modify _dataNames (Map.insert id name)
+    <<< Record.modify _dataShadows (Map.alter (maybe (Just 0) (Just <<< (1 + _))) name)
 
 incrementIndentation :: MetaContext -> MetaContext
 incrementIndentation = over MetaContext $ Record.modify _indentation (_ + 1)
