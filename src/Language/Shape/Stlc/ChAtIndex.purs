618ff9a3372cd3c34541e4ccf8e609ee53c6797d
Jacob Prinz <jacobeliasprinz@gmail.com>
2022-04-28 14:30:18 -0400

    Uses index recursor

--- src/Language/Shape/Stlc/ChAtIndex.purs
+++ src/Language/Shape/Stlc/ChAtIndex.purs
@@ -2,19 +2,30 @@ module Language.Shape.Stlc.ChAtIndex where
 
 import Prelude
 
-import Language.Shape.Stlc.Recursor.Context as Rec
+import Ansi.Codes (EscapeCode(..))
+import Data.List (List(..))
+import Data.Maybe (Maybe(..), isJust, maybe)
+import Language.Shape.Stlc.Index (IxDown(..), IxStep(..), IxStepLabel(..), ixStepBuf)
+import Language.Shape.Stlc.Recursor.Index (visitIxStep)
+import Language.Shape.Stlc.Recursor.Index as Rec
 import Undefined (undefined)
+import Unsafe (error)
 
-type Syntax = Unit
-
-chAtTerm :: Rec.ProtoRec Rec.ArgsTerm () (Unit -> Unit -> Boolean -> Unit)
-chAtTerm _ _ _ true = unit
-chAtTerm args _ _ false = Rec.recTerm {
-    lam : \args _ _ sbjoss -> undefined
+chAtTerm :: Rec.ProtoRec Rec.ArgsTerm () (Unit -> Unit -> Maybe Unit)
+chAtTerm {argsIx: {visit: {csr: Just (IxDown Nil)}}} _ _ = undefined -- pure unit
+chAtTerm args _ _ = Rec.recTerm {
+    lam : \args _ _ -> undefined
     , neu : undefined
     , let_ : undefined
-    , buf : undefined
+    , buf : \args _ _ ->
+        if isJust (args.argsIx.visit_term).csr then 
+            undefined
+        else if isJust (args.argsIx.visit_body).csr then
+            undefined
+        else
+        error "no"
+                            
     , data_ : undefined
     , match : undefined
     , hole : undefined
-} args unit unit false
+} args unit unit
