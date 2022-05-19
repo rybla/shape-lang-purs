073dd6a6b08da220f37ea5a520b6882d01bf99c3
Jacob Prinz <jacobeliasprinz@gmail.com>
2022-05-18 22:06:59 -0400

    numbered the nos

--- src/Language/Shape/Stlc/ChAtIndex.purs
+++ src/Language/Shape/Stlc/ChAtIndex.purs
@@ -41,17 +41,17 @@ chAtTerm args tRep idx = Rec.recTerm {
     (IxDown (IxStep IxStepLam 1 : rest)) -> do -- body of lambda
       body' /\ IxDown idx' /\ tc /\ holeEq <- chAtTerm args.body tRep (IxDown rest)
       pure $ Lam args.lam {body=body'} /\ IxDown (IxStep IxStepLam 1 : idx') /\ tc /\ holeEq
-    _ -> error "no"
+    _ -> error "no8"
   , neu : \args tRep -> case _ of
     (IxDown (IxStep IxStepNeu 1 : rest)) -> error "unimplemented" -- argument of neutral
-    _ -> error "no"
+    _ -> error "no9"
   , let_ : \args tRep -> case _ of
     (IxDown (IxStep IxStepLet 1 : rest)) -> do -- type of let
       ty' /\ IxDown idx' /\ tc /\ holeEq0 <- chAtType args.sign tRep (IxDown rest)
       let impl' /\ holeEq1 = runState (chTerm' args.impl emptyChanges tc) holeEq0
       let body' /\ holeEq2 = runState (chTerm' args.body (varChange emptyChanges args.termBind.termBind.termId tc) NoChange) holeEq1
       pure $ Let args.let_ {sign= ty', impl= impl', body= body'} /\ IxDown (ixStepLet.sign : idx') /\ NoChange /\ holeEq2
-    (IxDown (IxStep IxStepLet 2 : rest)) -> do -- definition of let
+    (IxDown ((IxStep IxStepLet 2) : rest)) -> do -- definition of let
       impl' /\ IxDown idx' /\ tc /\ holeEq1 <- chAtTerm args.impl tRep (IxDown rest)
       let sign' = applyTC tc args.sign.type_
       let body' /\ holeEq2 = runState (chTerm' args.body (varChange emptyChanges args.termBind.termBind.termId tc) NoChange) holeEq1
@@ -59,7 +59,7 @@ chAtTerm args tRep idx = Rec.recTerm {
     (IxDown (IxStep IxStepLet 3 : rest)) -> do -- body of let
       body' /\ IxDown idx' /\ tc /\ holeEq <- chAtTerm args.body tRep (IxDown rest)
       pure $ Let args.let_ {body = body'} /\ IxDown (ixStepLet.body : idx') /\ tc /\ holeEq
-    _ -> error "no"
+    _ -> error "no10"
   , buf : \args tRep -> case _ of
     (IxDown (IxStep IxStepBuf 0 : rest)) -> do -- type of thing in buffer
       ty' /\ IxDown idx' /\ tc /\ holeEq0 <- chAtType args.sign tRep (IxDown rest)
@@ -72,18 +72,18 @@ chAtTerm args tRep idx = Rec.recTerm {
     (IxDown (IxStep IxStepBuf 2 : rest)) -> do -- body of buffer (rest of program)
       body' /\ IxDown idx' /\ tc /\ holeEq <- chAtTerm args.body tRep (IxDown rest)
       pure $ Buf args.buf {body = body'} /\ IxDown (ixStepBuf.body : idx') /\ tc /\ holeEq
-    _ -> error "no"
+    _ -> error "no1"
   , data_ : \args tRep -> case _ of
     (IxDown (IxStep IxStepData 1 : rest)) -> error "unimplemented"
     (IxDown (IxStep IxStepData 2 : rest)) -> do -- body of data type definition
       body' /\ IxDown idx' /\ tc /\ holeEq <- chAtTerm args.body tRep (IxDown rest)
       pure $ Data args.data_ {body = body'} /\ IxDown (ixStepData.body : idx') /\ tc /\ holeEq
-    _ -> error "no"
+    _ -> error "no2"
   , match : \args tRep -> case _ of
     (IxDown (IxStep IxStepMatch 1 : rest)) -> error "unimplemented"
     (IxDown (IxStep IxStepMatch 2 : rest)) -> error "unimplemented"
-    _ -> error "no"
-  , hole : \args tRep -> case _ of _ -> error "no"
+    _ -> error "no3"
+  , hole : \args tRep -> case _ of _ -> error ("no4 bla " <> (show idx))
 } args tRep idx
 
 chAtType :: Record (Rec.ArgsType ()) -> ToReplace -> IxDown -> Maybe (Type /\ IxDown /\ TypeChange /\ HoleEq)
@@ -94,9 +94,9 @@ chAtType args tRep idx = Rec.recType {
       (dom' /\ (IxDown idx') /\ tcIn /\ holeEq) <- chAtType args.dom tRep (IxDown rest)
       pure $ ArrowType (args.arrowType {dom=dom'}) /\ IxDown ((IxStep IxStepArrowType 0) : idx') /\ ArrowCh tcIn NoChange /\ holeEq
     (IxDown (IxStep IxStepArrowType 1 : rest)) -> undefined
-    _ -> error "no"
+    _ -> error "no5"
   , dataType : \args tRep -> case _ of
-    _ -> error "no"
+    _ -> error "no6"
   , holeType : \args tRep -> case _ of
-    _ -> error "no"
+    _ -> error "no7"
 } args tRep idx
